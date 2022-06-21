5a33165e3f917f4877bffc58f04623a530839c22
maksimbuyanov <maksi_777@bk.ru>
2022-06-21 14:26:53 +0700

    add refreshButton, add 2 steps

--- src/Components/Game/Status/Status.jsx
+++ src/Components/Game/Status/Status.jsx
@@ -2,14 +2,20 @@ import React from "react"
 import "./Status.scss"
 import { useSelector } from "react-redux"
 import { getCafeName, getCash } from "../../../redux/initializing"
-import { getClients, getRounds } from "../../../redux/game"
+import { getClients, getRounds, getShowTable } from "../../../redux/game"
 import { moneySplitter } from "../../helpers/parser"
+import refreshSvg from "../../../image/refresh.svg"
 
 function Status() {
   const cash = useSelector(getCash)
   const round = useSelector(getRounds)
   const cafeName = useSelector(getCafeName)
   const clients = useSelector(getClients)
+  const isShowTable = useSelector(getShowTable)
+
+  const onClickRefresh = () => {
+    window.location.reload()
+  }
 
   return (
     <div className="game__status status">
@@ -18,6 +24,15 @@ function Status() {
         <div className="status__step">{round}</div>
         <div className="status__cash">{moneySplitter(cash)}</div>
         <div className="status__clients">{clients}</div>
+        {isShowTable && (
+          <button
+            className="status__refresh"
+            type="button"
+            onClick={onClickRefresh}
+          >
+            <img src={refreshSvg} alt="Обновить" className="status__img" />
+          </button>
+        )}
       </div>
     </div>
   )
